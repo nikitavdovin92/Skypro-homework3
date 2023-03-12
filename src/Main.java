7698701309526ce0b585a553b97c1adccb0a69ff
Homework3 <tradervdovin@gmail.com>
2023-03-12 13:18:23 +0500

    HW 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         Car car = new Car("car1", 4);
         Car car2 = new Car("car2", 4);
 
@@ -13,6 +14,7 @@ public class Main {
 
 
         ServiceStation station = new ServiceStation();
+
         station.check(car);
         station.check(car2);
         station.check(bicycle);
